db139f78aa6fd4fce47aebd727d6a9bfda80ce8b
Nevnet99 <luke-brannagan@hotmail.com>
2024-11-15 17:15:41 +0000

    feat: inital astro build

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Stckz';
+export const SITE_DESCRIPTION = 'Modern web development stacks, blog posts and tutorials.';
