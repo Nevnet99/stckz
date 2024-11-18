0d7a8873bd9d05e1daa99ec4452a69d5d65d7a5b
Callum Bonnyman <callum@wrux.com>
2024-11-18 13:34:31 +0700

    Code cleanup.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	daisyui: {
-		themes: ['night'],
-	},
-	plugins: [require('daisyui'), require('@tailwindcss/typography'),],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ['"Coven"', "sans-serif"],
+      "sans-outline": ['"Conven Outlined"', "sans-serif"],
+    },
+    extend: {},
+  },
+  daisyui: {
+    themes: ["night"],
+  },
+  plugins: [require("daisyui"), require("@tailwindcss/typography")],
+};
